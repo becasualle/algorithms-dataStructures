09587ed210fa12496a7b1aead9f9e0a54678987d
Mark <mark.nelubin@gmail.com>
2021-09-20 20:28:50 +0300

    SingleLinkedList Pop

---
+++ DataStructures/SinglyLinkedLists/singlyLinkedListPop.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.tail) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (!this.length) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current
+    }
+
+}
+
+const list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+console.log(list.pop());
+console.log(list);
