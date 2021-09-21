c2915fd2456aadf84ce37e417fa040dcbf013668
Mark <mark.nelubin@gmail.com>
2021-09-21 10:37:18 +0300

    Single Linked List Unshift

---
+++ DataStructures/SinglyLinkedLists/Unshift.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    unshift(val) {
+        const newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+
+    }
+
+}
+
+const list = new SinglyLinkedList();
+console.log(list.unshift(23))
+console.log(list.unshift(57))
