8709cc57aa21b5f027bfa284292bb525470924fd
Mark <mark.nelubin@gmail.com>
2021-09-21 10:17:33 +0300

    Single Linked List Shift

--- DataStructures/SinglyLinkedLists/Pop.js
+++ DataStructures/SinglyLinkedLists/Pop.js
@@ -27,19 +27,24 @@ class SinglyLinkedList {
 
     pop() {
         if (!this.head) return undefined;
+
         let current = this.head;
         let newTail = current;
+
         while (current.next) {
             newTail = current;
             current = current.next;
         }
+
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
+
         if (!this.length) {
             this.head = null;
             this.tail = null;
         }
+
         return current
     }
 
