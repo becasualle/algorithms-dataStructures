6d80f46e57eea3cfa0442ac8acc845c6048b633b
Mark <mark.nelubin@gmail.com>
2021-09-21 11:21:42 +0300

    Single Linked List Get

---
+++ DataStructures/SinglyLinkedLists/Get.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.tail) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) { return null };
+
+        let i = 0;
+        let current = this.head;
+
+        while (i !== index) {
+            current = current.next;
+            i++;
+        }
+
+        return current;
+    }
+}
+
+const list = new SinglyLinkedList();
+list.push(1)
+list.push(2)
+list.push(3)
+console.log(list.get(2))
