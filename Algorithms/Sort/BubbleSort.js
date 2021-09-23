36a368c57bce27ebd52d696299edc0128b1f0b8d
Mark <mark.nelubin@gmail.com>
2021-09-23 14:07:45 +0300

    Bubble Sort

---
+++ Algorithms/Sort/BubbleSort.js
@@ -0,0 +1,22 @@
+const bubbleSort = arr => {
+    let noSwaps;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j <= i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1])
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                noSwaps = false;
+            }
+        }
+        console.log('one pass complete')
+        if (noSwaps) break;
+    }
+    return arr
+}
+
+bubbleSort([5, 3, 4, 1, 2])
+bubbleSort([1, 6, 15, 33, 30, 32, 26, 36, 42, 45, 46, 47, 48])
+// [3, 1, 4, 2, 5]
