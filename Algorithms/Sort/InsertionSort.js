b4012b52eacc7788247b8c54416673f37f12f33f
Mark <mark.nelubin@gmail.com>
2021-09-24 18:57:28 +0300

    merge sort - merge function

--- Algorithms/Sort/InsertionSort.js
+++ Algorithms/Sort/InsertionSort.js
@@ -1,12 +1,12 @@
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         let currentVal = arr[i];
-        console.log({ currentVal, prevVal: arr[i - 1] })
+        // console.log({ currentVal, prevVal: arr[i - 1] })
         for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
             arr[j + 1] = arr[j];
-            console.log(arr);
+            // console.log(arr);
             arr[j] = currentVal;
-            console.log(arr);
+            // console.log(arr);
         }
     }
     return arr;
