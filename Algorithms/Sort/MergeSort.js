5959a9c9b672a97a32087c1ab42a6de916692007
Mark <mark.nelubin@gmail.com>
2021-09-24 20:03:01 +0300

    mergesort

--- Algorithms/Sort/MergeSort.js
+++ Algorithms/Sort/MergeSort.js
@@ -1,45 +1,33 @@
-const merge = (arr1, arr2) => {
-    let result = [];
+function merge(arr1, arr2) {
+    let results = [];
     let i = 0;
     let j = 0;
-
-    while (result.length < arr1.length + arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            result.push(arr1[i]);
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
             i++;
         } else {
-            result.push(arr2[j]);
+            results.push(arr2[j])
             j++;
         }
     }
-
-    return result;
+    while (i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
 }
 
-// Second approach
-// function merge(arr1, arr2){
-//     let results = [];
-//     let i = 0;
-//     let j = 0;
-//     while(i < arr1.length && j < arr2.length){
-//         if(arr2[j] > arr1[i]){
-//             results.push(arr1[i]);
-//             i++;
-//         } else {
-//             results.push(arr2[j])
-//             j++;
-//         }
-//     }
-//     while(i < arr1.length) {
-//         results.push(arr1[i])
-//         i++;
-//     }
-//     while(j < arr2.length) {
-//         results.push(arr2[j])
-//         j++;
-//     }
-//     return results;
-// }
+const mergeSort = (arr) => {
+    if (arr.length === 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right)
+}
 
-merge([100, 200], [1, 2, 3, 5, 6])
-merge([1, 10, 50], [2, 14, 99, 100]);
+console.log(mergeSort([10, 24, 76, 73]));
