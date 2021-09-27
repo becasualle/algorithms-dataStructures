a0ea480cd4935bfcc830b009c3f62bbd16b1f5c6
Mark <mark.nelubin@gmail.com>
2021-09-27 10:15:08 +0300

    Quick Sort

--- Algorithms/Sort/QuickSort.js
+++ Algorithms/Sort/QuickSort.js
@@ -23,7 +23,7 @@ const pivot = (arr, startIdx = 0, endIdx = arr.length - 1) => {
     return pivotIdx;
 }
 
-pivot([4, 8, 2, 1, 5, 7, 6, 3]) // 3
+// pivot([4, 8, 2, 1, 5, 7, 6, 3]) // 3
 // start, pivotIndex = 0  [4, 8, 2, 1, 5, 7, 6, 3]
 // i=1, pivotIndex = 0 [4, 8, 2, 1, 5, 7, 6, 3]
 // i=2, pivotIndex = 1 [4, 2, 8, 1, 5, 7, 6, 3]
@@ -34,3 +34,24 @@ pivot([4, 8, 2, 1, 5, 7, 6, 3]) // 3
 // i=7, pivotIndex = 3 [4, 2, 1, 3, 5, 7, 6, 8]
 
 // swap pivot with pivotIndex [3, 2, 1, 4, 5, 7, 6, 8]
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+
+    if (left < right) {
+        const pivotIdx = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIdx - 1);
+        // right
+        quickSort(arr, pivotIdx + 1, right)
+    }
+    return arr
+}
+
+quickSort([4, 6, 9, 1, 2, 5, 3])
+// [4, 6, 9, 1, 2, 5, 3]
+// [3, 1, 2, 4, 9, 5, 6]
+//           4
+// 3, 2, 1      6, 9, 5
+//       3         6
+// 2, 1         5     9
+// 2
