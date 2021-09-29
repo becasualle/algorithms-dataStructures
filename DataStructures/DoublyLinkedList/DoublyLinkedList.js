399e6922a3ed7468836aa280767a1b4fc0f73a8c
Mark <mark.nelubin@gmail.com>
2021-09-29 10:39:30 +0300

    Doubly Linked List Push

--- DataStructures/DoublyLinkedList/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList/DoublyLinkedList.js
@@ -42,6 +42,22 @@ class DoublyLinkedList {
 
         return temp;
     }
+    shift() {
+        if (!this.head) return undefined;
+
+        const temp = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+        this.length--;
+        return temp;
+    }
 }
 
 const list = new DoublyLinkedList();
