8f68808c3a3331aa819c5e99996cb53b72fc198a
Mark <mark.nelubin@gmail.com>
2021-09-29 11:08:46 +0300

    Doubly Linked List unshift

--- DataStructures/DoublyLinkedList/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList/DoublyLinkedList.js
@@ -58,6 +58,20 @@ class DoublyLinkedList {
         this.length--;
         return temp;
     }
+    unshift(val) {
+        const newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 const list = new DoublyLinkedList();
